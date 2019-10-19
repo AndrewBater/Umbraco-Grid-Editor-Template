5c22b04551eda91794f5b146966606c1e6a3face
Andrew Bater <andrewbater@googlemail.com>
2019-10-19 14:23:37 +0100

    Include $scope and value by default

--- package/editor.controller.js
+++ package/editor.controller.js
@@ -1,9 +1,12 @@
 (function () {
     "use strict";
 
-    function $fileinputname$EditorController() {
+    function $fileinputname$EditorController($scope) {
         var vm = this;
 
+        if (!$scope.control.value) {
+            $scope.control.value = {};
+        }
     }
 
     angular.module("umbraco").controller("$name$.$fileinputname$EditorController", $fileinputname$EditorController);
